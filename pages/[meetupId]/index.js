9f103d49501fa76a3463478bcec560bfdd407ab7
Hessam <hessam.imen@gmail.com>
2023-11-29 20:42:21 +1100

    feat: implement getStaticProps from dynamic pages with getStaticPaths and falback:false

--- pages/[meetupId]/index.js
+++ pages/[meetupId]/index.js
@@ -4,11 +4,46 @@ function MeetupDetails() {
   return (
     <MeetupDetail
       image="https://buffer.com/cdn-cgi/image/w=1000,fit=contain,q=90,f=auto/library/content/images/size/w600/2023/10/free-images.jpg"
-      title="Frist Meetup"
+      title="First Meetup"
       address="5 some street, Sydney, 2000"
       description="the meetup description"
     />
   );
+}
+
+export async function getStaticPaths() {
+  return {
+    fallback: false,
+    paths: [
+      {
+        params: {
+          meetupId: "m1",
+        },
+      },
+      {
+        params: {
+          meetupId: "m2",
+        },
+      },
+    ],
+  };
+}
+
+export async function getStaticProps(context) {
+  const meetupId = context.params.meetupId;
+  //fetch data for a single meetup
+  return {
+    props: {
+      meetupData: {
+        id: meetupId,
+        image:
+          "https://buffer.com/cdn-cgi/image/w=1000,fit=contain,q=90,f=auto/library/content/images/size/w600/2023/10/free-images.jpg",
+        title: "Frist Meetup",
+        address: "5 some street, Sydney, 2000",
+        description: "the meetup description",
+      },
+    },
+  };
 }
 
 export default MeetupDetails;
