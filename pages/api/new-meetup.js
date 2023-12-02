d285c8a763d834d14a81b84edd55bffcefcf6517
Hessam <hessam.imen@gmail.com>
2023-12-02 12:35:48 +1100

    feat: connect and query a MongoDB database

--- pages/api/new-meetup.js
+++ pages/api/new-meetup.js
@@ -1,7 +1,20 @@
-function handler(req, res) {
+import { MongoClient } from "mongodb";
+
+async function handler(req, res) {
   if (req.method === "POST") {
     const data = req.body;
-    const { title, image, address, description } = data;
+
+    const client = await MongoClient.connect(
+      "mongodb+srv://hessam:<samplepassword>@cluster0.9dsbga4.mongodb.net/meetups?retryWrites=true&w=majority"
+    );
+
+    const db = client.db();
+
+    const meetupsCollection = db.collection("meetups");
+
+    const result = await meetupsCollection.insertOne(data);
+
+    console.log(result);
   }
 }
 
