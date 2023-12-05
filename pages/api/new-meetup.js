e7018c34f5d07fd68ceec2fef0c561a533f1553e
Hessam <hessam.imen@gmail.com>
2023-12-05 16:27:24 +1100

    feat: sending http requests to api routes

--- pages/api/new-meetup.js
+++ pages/api/new-meetup.js
@@ -5,7 +5,7 @@ async function handler(req, res) {
     const data = req.body;
 
     const client = await MongoClient.connect(
-      "mongodb+srv://hessam:<samplepassword>@cluster0.9dsbga4.mongodb.net/meetups?retryWrites=true&w=majority"
+      "mongodb+srv://hessam:samplepassword@cluster0.9dsbga4.mongodb.net/meetups?retryWrites=true&w=majority"
     );
 
     const db = client.db();
@@ -15,6 +15,10 @@ async function handler(req, res) {
     const result = await meetupsCollection.insertOne(data);
 
     console.log(result);
+
+    client.close();
+
+    res.status(201).json({ message: "Meetup Inserted" });
   }
 }
 
