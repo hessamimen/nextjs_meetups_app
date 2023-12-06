69661d28c04eed9ad017e3b833a2c0249d4afa83
Hessam <hessam.imen@gmail.com>
2023-12-07 07:21:13 +1100

    feat: get meetup details and path dynamically from the mongodb backend

--- pages/[meetupId]/index.js
+++ pages/[meetupId]/index.js
@@ -1,46 +1,63 @@
+import { MongoClient, ObjectId } from "mongodb";
 import MeetupDetail from "../../components/meetups/MeetupDetail";
 
-function MeetupDetails() {
+function MeetupDetails(props) {
   return (
     <MeetupDetail
-      image="https://buffer.com/cdn-cgi/image/w=1000,fit=contain,q=90,f=auto/library/content/images/size/w600/2023/10/free-images.jpg"
-      title="First Meetup"
-      address="5 some street, Sydney, 2000"
-      description="the meetup description"
+      image={props.meetupData.image}
+      title={props.meetupData.title}
+      address={props.meetupData.address}
+      description={props.meetupData.description}
     />
   );
 }
 
 export async function getStaticPaths() {
+  const client = await MongoClient.connect(
+    "mongodb+srv://hessam:samplepassword@cluster0.9dsbga4.mongodb.net/meetups?retryWrites=true&w=majority"
+  );
+
+  const db = client.db();
+
+  const meetupsCollection = db.collection("meetups");
+
+  const meetups = await meetupsCollection.find({}, { _id: 1 }).toArray();
+
+  client.close();
+
   return {
     fallback: false,
-    paths: [
-      {
-        params: {
-          meetupId: "m1",
-        },
-      },
-      {
-        params: {
-          meetupId: "m2",
-        },
-      },
-    ],
+    paths: meetups.map((meetup) => ({
+      params: { meetupId: meetup._id.toString() },
+    })),
   };
 }
 
 export async function getStaticProps(context) {
   const meetupId = context.params.meetupId;
   //fetch data for a single meetup
+
+  const client = await MongoClient.connect(
+    "mongodb+srv://hessam:samplepassword@cluster0.9dsbga4.mongodb.net/meetups?retryWrites=true&w=majority"
+  );
+
+  const db = client.db();
+
+  const meetupsCollection = db.collection("meetups");
+
+  const selectedMeetup = await meetupsCollection.findOne({
+    _id: new ObjectId(meetupId),
+  });
+
+  client.close();
   return {
     props: {
       meetupData: {
-        id: meetupId,
-        image:
-          "https://buffer.com/cdn-cgi/image/w=1000,fit=contain,q=90,f=auto/library/content/images/size/w600/2023/10/free-images.jpg",
-        title: "Frist Meetup",
-        address: "5 some street, Sydney, 2000",
-        description: "the meetup description",
+        id: selectedMeetup._id.toString(),
+        title: selectedMeetup.title.toString(),
+        image: selectedMeetup.image.toString(),
+        address: selectedMeetup.address.toString(),
+        description: selectedMeetup.description.toString(),
       },
     },
   };
